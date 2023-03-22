e4ae0de4e29eea9f6bf036dcb6b5e8813abc3b09
Simone Bergonzi <simemablak@gmail.com>
2023-03-22 23:13:56 +0100

    Change Skeleton sprite

--- Roguelike/Assets/Scripts/EnemyController.cs
+++ Roguelike/Assets/Scripts/EnemyController.cs
@@ -9,13 +9,14 @@ public class EnemyController : MonoBehaviour
 
     public float rangeOfVision = 8;
     private Vector3 movementDirection;
+    private float originX;
 
     public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        originX = transform.localScale.x;
     }
 
     // Update is called once per frame
@@ -37,5 +38,14 @@ public class EnemyController : MonoBehaviour
             animator.SetBool("isMoving", true);
         else
             animator.SetBool("isMoving", false);
+
+        if (movementDirection.x > 0)
+        {
+            transform.localScale = new Vector3(-1f * originX, transform.localScale.y, transform.localScale.z);
+        }
+        else if (movementDirection.x < 0)
+        {
+            transform.localScale = new Vector3(originX, transform.localScale.y, transform.localScale.z);
+        }
     }
 }
